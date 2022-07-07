0d4f2750d1b142002df3cbd898da5650052a2d2d
Hasan-Tunc <hasantunc.dev@gmail.com>
2022-07-08 00:28:16 +0300

    Feat : Adding new animations to character

---
+++ Assets/_Main/Scripts/FPSSetter.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FPSSetter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 120;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
